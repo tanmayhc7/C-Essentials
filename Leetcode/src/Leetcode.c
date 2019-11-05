ec8eef8c3cd762e723dd25c59e44f80bcc4b062b
tanmayhc7 <tanmayhc@outlook.com>
2019-11-05 10:58:07 -0800

    .

---
+++ Leetcode/src/Leetcode.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Leetcode.c
+ Author      : Tanmay
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+}
