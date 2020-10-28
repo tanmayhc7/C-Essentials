d3d0f818aba62c4367614479dbebb06809781bd2
Tanmay Chandavarkar <56776651+tanmayhc7@users.noreply.github.com>
2020-10-28 16:37:04 -0700

    Update moveZeroes(283).c

--- Leetcode/Algo/Easy/moveZeroes(283).c
+++ Leetcode/Algo/Easy/moveZeroes(283).c
@@ -1,20 +1,23 @@
 /*
  * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
  */
-void moveZeroes(int* nums, int N)
-{
-    int totalZeros = 0;
+void swap(int* a,int* b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;    
+}
 
-    for(int i = 0; i < N; i++) {
-        if(nums[i] == 0) {
-            totalZeros++;
-        }
-        else {
-            nums[i - totalZeros] = nums[i];
-        }
-    }
-
-    for(int i = (N - totalZeros); i < N; i++) {
-        nums[i] = 0;
-    }
+void moveZeroes(int* nums, int numsSize){
+    int l=0,r=1;
+    
+    while(r<numsSize){
+        if(nums[l]==0 && nums[r]!=0){
+            swap(&nums[l],&nums[r]);
+            l++;r++;
+        } else if((nums[l]!=0 && nums[r]!=0) || (nums[l]!=0 && nums[r]==0)){
+            l++;r++;
+        } else{
+            r++;
+        }   
+    }   
 }
